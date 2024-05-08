8484dfdbf33d221c6dbab85d0d6d6511cbc6de14
Farid <makflay85zet@gmail.com>
2024-05-08 19:14:14 +0400

    lesson5 changed Tree

--- 05/lesson5/src/main/java/org/example/Tree.java
+++ 05/lesson5/src/main/java/org/example/Tree.java
@@ -25,30 +25,13 @@ public class Tree {
         }
         System.out.println(file.getName());
 
-//        if (file.isDirectory()) {
             File[] files = file.listFiles();
             if (files != null) {
                 for (int i = 0; i < files.length; i++) {
                     print(files[i], indent, i == files.length - 1);
                 }
-//            }
         }
 
-//        File[] files = file.listFiles();
-//
-//        if (files != null) {     // Добавил проверку на null и удалил проверку на директории
-//
-//            int subDirTotal = 0;
-//
-//            for (int i = 0; i < files.length; i++) {
-//                    subDirTotal++;
-//            }
-//
-//            int subDirCounter = 0;
-//            for (int i = 0; i < files.length; i++) {
-//                    print(files[i], indent, subDirTotal == ++subDirCounter);
-//            }
-//        }
 
     }
 
